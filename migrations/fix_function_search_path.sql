112c03759c96a64b9e78f90d68ddf129fca54cbc
Kons <konscodes@icloud.com>
2025-12-08 22:39:09 +0900

    Crearing unused scripts

--- migrations/fix_function_search_path.sql
+++
@@ -1,9 +0,0 @@
--- Fix function search_path security issue
--- This prevents potential SQL injection via search_path manipulation
--- Reference: https://supabase.com/docs/guides/database/database-linter?lint=0011_function_search_path_mutable
-
--- Fix search_path for ID generation functions
-ALTER FUNCTION public.next_job_id() SET search_path = public;
-ALTER FUNCTION public.next_client_id() SET search_path = public;
-ALTER FUNCTION public.next_order_id() SET search_path = public;
-
